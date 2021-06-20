bc472bb063e9237dceb783f0052cc427ff4d1b59
Adam Dziomdziora <190140@edu.p.lodz.pl>
2021-06-20 19:39:16 +0200

    test of update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bio-medical-cloud';
+
+  get getFullYear(): number {
+    const date = new Date();
+    return date.getFullYear();
+  }
 }
