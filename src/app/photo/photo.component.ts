768fcfb9ad69b9a67e8535ed47b98dc4117517c3
Adam Dziomdziora <190140@edu.p.lodz.pl>
2021-07-23 14:42:16 +0200

    updates

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,6 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { BlobServiceClient, ContainerClient } from '@azure/storage-blob';
-import * as FileSaver from 'file-saver';
+import FileSaver from 'file-saver';
+
+interface ISasReponse {
+  sasToken: string;
+  responseMessage: string;
+}
 
 @Component({
   selector: 'app-photo',
@@ -16,24 +22,41 @@ export class PhotoComponent implements AfterViewInit {
 
   width = 640;
   height = 480;
-
-  captures: string[] = [];
-  error: any;
+  sasToken = '';
   isCaptured = false;
-  myBlob!: Blob;
-  account = 'mysuperblob';
-  containerName = 'images';
-  url =
-    // eslint-disable-next-line max-len
-    'https://mysuperblob.blob.core.windows.net/?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacuptfx&se=2021-07-10T20:34:42Z&st=2021-07-09T12:34:42Z&spr=https,http&sig=zZ5N3bGTugjaVOPvmCQpi1PMBsvnUu7z86HA%2FWtnDi4%3D';
-  blobServiceClient = new BlobServiceClient(this.url);
-  containerClient!: ContainerClient;
+  error = '';
+  latestUploadedUrl = '';
+
+  private captures: string[] = [];
+  private containerName = 'images';
+  private url = 'https://mysuperblob.blob.core.windows.net/';
+  private blobServiceClient = new BlobServiceClient(this.url);
+  private containerClient!: ContainerClient;
+  private sasUrl =
+    'https://biomedicalappsastoken.azurewebsites.net/api/HttpTriggerGetSasToken';
+  /**
+   *
+   */
+
+  constructor(private httpClient: HttpClient) {}
+  public async getSasToken() {
+    const response = await this.httpClient
+      .get<ISasReponse>(this.sasUrl)
+      .toPromise();
+    this.sasToken = response.sasToken;
+    this.blobServiceClient = new BlobServiceClient(this.url + this.sasToken);
+  }
 
   async ngAfterViewInit() {
+    await this.getSasToken();
     await this.setupDevices();
     this.containerClient = this.blobServiceClient.getContainerClient(
       this.containerName
     );
+  }
+
+  goToLink() {
+    window.open(this.latestUploadedUrl);
   }
 
   async setupDevices() {
@@ -45,7 +68,7 @@ export class PhotoComponent implements AfterViewInit {
         if (stream) {
           this.video.nativeElement.srcObject = stream;
           this.video.nativeElement.play();
-          this.error = null;
+          this.error = '';
         } else {
           this.error = 'You have no output video device';
         }
@@ -68,7 +91,6 @@ export class PhotoComponent implements AfterViewInit {
     const canvasHtml: HTMLCanvasElement = this.canvas.nativeElement;
     canvasHtml.toBlob(async (blob) => {
       if (blob) {
-        this.myBlob = blob;
         const blobName = 'photo' + new Date().getTime();
         const blockBlobClient =
           this.containerClient.getBlockBlobClient(blobName);
@@ -84,6 +106,22 @@ export class PhotoComponent implements AfterViewInit {
         await this.downloadUploadedFromAzure();
       }
     });
+  }
+
+  public get isMobile(): boolean {
+    const toMatch = [
+      /Android/i,
+      /webOS/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i,
+    ];
+
+    return toMatch.some((toMatchItem) =>
+      navigator.userAgent.match(toMatchItem)
+    );
   }
 
   async previewImage(event: any) {
@@ -103,7 +141,10 @@ export class PhotoComponent implements AfterViewInit {
       `Uploaded block blob ${blobName} successfully`,
       uploadBlobResponse.requestId
     );
-    await this.downloadUploadedFromAzure();
+    this.latestUploadedUrl = blockBlobClient.url;
+    if (!this.isMobile) {
+      await this.downloadUploadedFromAzure();
+    }
   }
 
   private async downloadUploadedFromAzure() {
